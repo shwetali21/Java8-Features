97d293dde560b9c875f46340de4305784bf71ac9
ASUS <ASUS@DESKTOP-FQPIVJD>
2019-02-21 18:41:55 +0530

    Method Reference program

---
+++ src/com/corejava/java8/features/methodreference/MethodReferenceForNew.java
@@ -0,0 +1,12 @@
+package com.corejava.java8.features.methodreference;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class MethodReferenceForNew {
+	
+	public static void main(String[] args) {
+		//Set<String>  set = HashSet<String>::new;
+	}
+	
+}
